4e11bb8f28eb3960665845c6b200cb3e62951f39
guicrispa <guicrispim1000@gmail.com>
2022-12-29 12:14:46 -0300

    text places done

--- footer.php
+++ footer.php
@@ -1,1 +1,10 @@
-<h1>corinthians</h1>
+<style>
+    footer {
+        background: #1D2A3F;
+        padding: 30px 80px;
+        color: #f3fafa;
+    }
+</style>
+<footer>
+    <h1 class="teste">corinthians</h1>
+</footer>
--- index.php
+++ index.php
@@ -19,7 +19,7 @@
     <meta name="description" content="Guilherme Crispim - Site de lugares onde quero viajar usando HTML, CSS e JS">
     <title>Minhas futuras viagens</title>
     <link rel="stylesheet" href="css/style.css"> 
-    <link rel="icon" href="https://res.cloudinary.com/dlsuopwkn/image/upload/v1671721545/umjovemincentivandojovens.png">
+    <link rel="icon" href="https://res.cloudinary.com/dlsuopwkn/image/upload/v1671721450/logo_feecor.png">
 </head>
 <body>
     <header>
@@ -30,22 +30,22 @@
                 <li>Brasil</li>
                 <li>Austrália</li>
                 <li>Islândia</li>
-                <li>Uruguai</li>
+                <li>Canadá</li>
             </ul>
         </div>
     </header>
     <section id="section0">
-
+        <h1>Apresentação</h1>
     </section>
     <section id="section1">
         <div class="container">
             <div class="card">
                 <div class="imgBox">
-                    <img src="https://res.cloudinary.com/dlsuopwkn/image/upload/v1671675718/preikestolen.jpg" alt="preikestolen">
+                    <img src="https://res.cloudinary.com/dlsuopwkn/image/upload/c_scale,h_3264,w_4928/v1671675718/preikestolen.jpg" alt="preikestolen">
                 </div>
                 <div class="content">
                     <h2>Preikestolen</h2>
-                    <p>Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum</p>
+                    <p>Também é conhecida como Pulpit Rock, Preikestolen é uma falésia de 604 metros de desnível que se ergue sobre o Fiorde de Lyse, em frente ao platô Kjerag, em Forsand, no sudoeste da Noruega.</p>
                 </div>
             </div>
             <div class="card">
@@ -54,53 +54,52 @@
                 </div>
                 <div class="content">
                     <h2>Trolltunga</h2>
-                    <p>Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum</p>
+                    <p>Trolltunga é uma rocha horizontal esticada a 700 metros de altura, no sudoeste da Noruega. O nome significa Língua do Troll, pela semelhança da rocha com uma língua estendida.</p>
 
                 </div>
             </div>
             <div class="card">
                 <div class="imgBox">
-                    <img src="https://res.cloudinary.com/dlsuopwkn/image/upload/v1672024199/tromso.jpg" alt="Tromsø">
+                    <img src="https://res.cloudinary.com/dlsuopwkn/image/upload/c_scale,h_3264,w_4928/v1672024199/tromso.jpg" alt="Tromsø">
                 </div>
                 <div class="content">
                     <h2>Tromsø</h2>
-                    <p>Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum</p>
+                    <p>Tromsø, a linda cidade de incríveis fenômenos da natureza como a Aurora Boreal e do sol da meia-noite, é a maior cidade do norte da Noruega e está localizada no lado leste da ilha de Tromsøya</p>
                 </div>
             </div>
 
             <div class="card">
                 <div class="imgBox">
-                    <img src="https://res.cloudinary.com/dlsuopwkn/image/upload/v1672024197/kjeragbolten.jpg" alt="kjeragbolten">
+                    <img src="https://res.cloudinary.com/dlsuopwkn/image/upload/c_scale,h_3264,w_4928/v1672024197/kjeragbolten.jpg" alt="kjeragbolten">
                 </div>
                 <div class="content">
                     <h2>Kjeragbolten</h2>
-                    <p>Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum</p>
+                    <p>Kjeragbolten é uma rocha de 5 m³, presa em uma fenda das montanhas Kjerag, conhecida por trilhas e escaladas, a 1.084 metros acima do fiorde Lysefjorden, localizada no sul da Noruega.</p>
                 </div>
             </div>
 
             <div class="card">
                 <div class="imgBox">
-                    <img src="https://res.cloudinary.com/dlsuopwkn/image/upload/v1672024309/Atlanterhavsvegen.jpg" alt="Atlanterhavsvegen">
+                    <img src="https://res.cloudinary.com/dlsuopwkn/image/upload/c_scale,h_3264,w_4928/v1672024309/Atlanterhavsvegen.jpg" alt="Atlanterhavsvegen">
                 </div>
                 <div class="content">
                     <h2>Atlanterhavsvegen</h2>
-                    <p>Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum</p>
+                    <p>Atlanterhavsvegen, a Estrada do Atlântico, é uma experiência única que fica no Norte da Noruega com pontes que se arqueiam entre as ilhotas e recifes ao longo da áspera Baía de Hustadvika.</p>
                 </div>
             </div>
 
             <div class="card">
                 <div class="imgBox">
-                    <img src="https://res.cloudinary.com/dlsuopwkn/image/upload/v1672024197/snowhotel.jpg" alt="snowhotel">
+                    <img src="https://res.cloudinary.com/dlsuopwkn/image/upload/v1672321549/lofoten_soccerfield.png" alt="Lofoten Islands">
                 </div>
                 <div class="content">
-                    <h2>Snowhotel Kirkenes</h2>
-                    <p>Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum Lorem impsum</p>
+                    <h2>Lofoten Islands</h2>
+                    <p>Localizadas em Nordland e bem acima do círculo ártico, as Ilhas Lofoten têm paisagens incríveis, formadas pelos oceanos, picos acidentados, praias de areia branca e vilas pesqueiras.
+</p>
                 </div>
             </div>
         </div>
     </section>
-
-    <section>teste</section>
 
     <?php include('footer.php'); ?>
 
