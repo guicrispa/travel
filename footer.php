b777722564de0786b968d1f3fc31fc97531a1d68
guicrispa <guicrispim1000@gmail.com>
2022-12-30 12:13:56 -0300

    footer done and skip to main content link button

--- footer.php
+++ footer.php
@@ -3,8 +3,63 @@
         background: #1D2A3F;
         padding: 30px 80px;
         color: #f3fafa;
+        border-top: 2px solid #000;
+    }
+
+    footer p,
+    .teste {
+        text-align: center;
+        font-weight: 600;
+    }
+
+    footer p ion-icon {
+        background: #272a2f;
+        color: #fff;
+        padding: 15px;
+        box-shadow: 0 5px 20px rgba(0, 0, 0, 0.5);
+        font-size: 26px;
+        margin: 0 10px;
+        margin-top: 20px;
+        border-radius: 6px;
+    }
+
+    footer p ion-icon:hover {
+        background: #fff;
+        color: #272a2f;
+        transition: 0.5s;
+        cursor: pointer;
+    }
+
+    /* section copyright */
+
+    .copyright {
+        border-top: 1px solid #A0C8F9;
+    }
+    .copyright, .copyright p {
+        background: #272a2f;
+        padding: 2px;
+        text-align: center;
+        color: #fff;
+        font-weight: 600;
     }
 </style>
 <footer>
-    <h1 class="teste">corinthians</h1>
+    <p>
+        <h2 class="teste">Meios de contato:</h1>
+    </p>
+    <p>
+        <a title="Github Guilherme Crispim" href="https://github.com/guicrispa" target="_blank"><ion-icon name="logo-github"></ion-icon></a>
+        <a title="Linkedin Guilherme Crispim" href="https://br.linkedin.com/in/guilherme-crispim" target="_blank"><ion-icon name="logo-linkedin"></ion-icon></a>
+        <a title="Instagram Guilherme Crispim" href="https://instagram.com/gui.crispaa" target="_blank"><ion-icon name="logo-instagram"></ion-icon></a>
+        <a title="Behance Guilherme Crispim" href="https://www.behance.net/guilhermesilva77" target="_blank"><ion-icon name="logo-behance"></ion-icon></a>
+        <a title="E-mail Guilherme Crispim" href="mailto:guicrispim1000@gmail.com" target="_blank"><ion-icon name="mail"></ion-icon></a>
+    </p>
 </footer>
+<div class="copyright">
+    <p>&copy; Guilherme Crispim - Todos os Direitos Reservados</p>
+</div>
+
+</body>
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script src="js/script.js"></script>
+</html>
---
+++ head.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <!-- meta tags facebook open graph -->
+    <meta property="og:title" content="Lugares onde quero viajar!">
+    <meta property="og:site_name" content="Minhas futuras viagens">
+    <meta property="og:type" content="website">
+    <meta property="og:locale" content="pt_BR">
+    <!-- meta tags de imagens -->
+    <meta property="og:image" content="https://res.cloudinary.com/dlsuopwkn/image/upload/v1671675718/preikestolen.jpg">
+    <meta property="og:image:type" content="image/jpg">
+    <meta property="og:image:alt" content="Preikestolen">
+    <meta property="og:image:width" content="250">
+    <meta property="og:image:height" content="250">
+    <!--  tags de meta, título da pág, logo da pág e arquivo css -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Guilherme Crispim - Site de lugares onde quero viajar usando HTML, CSS e JS">
+    <title>Minhas futuras viagens</title>
+    <link rel="stylesheet" href="css/style.css"> 
+    <link rel="icon" href="https://res.cloudinary.com/dlsuopwkn/image/upload/v1672401895/logoicon.png">
+</head>
+<body>
+    <a href="#main" class="skip-to-main-content-link">Saltar para o conteúdo principal</a>
