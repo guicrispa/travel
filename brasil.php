e79163cf02de25122462c1f60d3937a6cc39e398
Guilherme Crispim <guicrispim1000@gmail.com>
2023-02-07 00:04:05 -0300

    cities brazil

--- brasil.php
+++ brasil.php
@@ -37,7 +37,7 @@
         </div>
     </section>
     <section id="section1title">
-        <h1>Lugares insanos daqui que mais quero conhecer :)</h1>
+        <h1>As cidades do nosso país que mais quero conhecer :)</h1>
     </section>
     <section id="section1">
         <div class="container">
@@ -46,7 +46,7 @@
                     <img src="https://res.cloudinary.com/dlsuopwkn/image/upload/c_scale,h_3264,w_4928/v1671675718/preikestolen.jpg" alt="preikestolen">
                 </div>
                 <div class="content">
-                    <h2>Preikestolen</h2>
+                    <h2>Campos do Jordão, SP</h2>
                     <p>Também é conhecida como Pulpit Rock, Preikestolen é uma falésia de 604 metros de desnível que se ergue sobre o Fiorde de Lyse, em frente ao platô Kjerag, em Forsand, no sudoeste da Noruega.</p>
                 </div>
             </div>
@@ -55,7 +55,7 @@
                     <img src="https://res.cloudinary.com/dlsuopwkn/image/upload/v1672024200/Trolltunga.jpg" alt="trolltunga">
                 </div>
                 <div class="content">
-                    <h2>Trolltunga</h2>
+                    <h2>Canela, RS</h2>
                     <p>Trolltunga é uma rocha horizontal esticada a 700 metros de altura, no sudoeste da Noruega. O nome significa Língua do Troll, pela semelhança da rocha com uma língua estendida.</p>
 
                 </div>
@@ -65,7 +65,7 @@
                     <img src="https://res.cloudinary.com/dlsuopwkn/image/upload/c_scale,h_3264,w_4928/v1672024199/tromso.jpg" alt="Tromsø">
                 </div>
                 <div class="content">
-                    <h2>Tromsø</h2>
+                    <h2>Gramado, RS</h2>
                     <p>Tromsø, a linda cidade de incríveis fenômenos da natureza como a Aurora Boreal e do sol da meia-noite, é a maior cidade do norte da Noruega e está localizada ao leste da ilha de Tromsøya.</p>
                 </div>
             </div>
@@ -75,7 +75,7 @@
                     <img src="https://res.cloudinary.com/dlsuopwkn/image/upload/c_scale,h_3264,w_4928/v1672024197/kjeragbolten.jpg" alt="kjeragbolten">
                 </div>
                 <div class="content">
-                    <h2>Kjeragbolten</h2>
+                    <h2>Maringá, SC</h2>
                     <p>Kjeragbolten é uma rocha de 5 m³, presa em uma fenda das montanhas Kjerag, conhecida por trilhas e escaladas, a 1.084 metros acima do fiorde Lysefjorden, localizada no sul da Noruega.</p>
                 </div>
             </div>
@@ -85,7 +85,7 @@
                     <img src="https://res.cloudinary.com/dlsuopwkn/image/upload/c_scale,h_3264,w_4928/v1672024309/Atlanterhavsvegen.jpg" alt="Atlanterhavsvegen">
                 </div>
                 <div class="content">
-                    <h2>Atlanterhavsvegen</h2>
+                    <h2>Florianópolis, SC</h2>
                     <p>Atlanterhavsvegen, a Estrada do Atlântico, é uma experiência única que fica no Norte da Noruega com pontes que se arqueiam entre as ilhotas e recifes ao longo da áspera Baía de Hustadvika.</p>
                 </div>
             </div>
@@ -95,9 +95,28 @@
                     <img src="https://res.cloudinary.com/dlsuopwkn/image/upload/v1672321549/lofoten_soccerfield.png" alt="Lofoten Islands">
                 </div>
                 <div class="content">
-                    <h2>Lofoten Islands</h2>
-                    <p>Localizadas em Nordland e bem acima do círculo ártico, as Ilhas Lofoten têm paisagens incríveis, formadas pelos oceanos, picos acidentados, praias de areia branca e vilas pesqueiras.
-</p>
+                    <h2>Lençóis Maranhenses</h2>
+                    <p>Localizadas em Nordland e bem acima do círculo ártico, as Ilhas Lofoten têm paisagens incríveis, formadas pelos oceanos, picos acidentados, praias de areia branca e vilas pesqueiras.</p>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="imgBox">
+                    <img src="https://res.cloudinary.com/dlsuopwkn/image/upload/v1672321549/lofoten_soccerfield.png" alt="Lofoten Islands">
+                </div>
+                <div class="content">
+                    <h2>Balneário, SC</h2>
+                    <p>Localizadas em Nordland e bem acima do círculo ártico, as Ilhas Lofoten têm paisagens incríveis, formadas pelos oceanos, picos acidentados, praias de areia branca e vilas pesqueiras.</p>
+                </div>
+            </div>
+
+            <div class="card">
+                <div class="imgBox">
+                    <img src="https://res.cloudinary.com/dlsuopwkn/image/upload/v1672321549/lofoten_soccerfield.png" alt="Lofoten Islands">
+                </div>
+                <div class="content">
+                    <h2>Petrópolis, RJ</h2>
+                    <p>Localizadas em Nordland e bem acima do círculo ártico, as Ilhas Lofoten têm paisagens incríveis, formadas pelos oceanos, picos acidentados, praias de areia branca e vilas pesqueiras.</p>
                 </div>
             </div>
         </div>
